375567c61a4889d492b2f394f799a92c4729308c
Jengas2015 <arejang@gmail.com>
2023-02-21 08:14:04 -0600

    code 12
    vowel finder.
    split, forEach,

---
+++ codes011-020/code12/main.js
@@ -0,0 +1,22 @@
+/* Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces. */
+
+function getCount(str) {
+    
+    let strArray = str.split('')
+    let vowelCount= 0
+    strArray.forEach(letter => {
+        if(letter==='a'|| letter==='e'|| letter==='i' || letter==='o' || letter==='u'){
+            vowelCount++
+        }
+    })
+    return vowelCount
+  }
+
+
+  testStr='camelcase'
+
+  getCount(testStr)
