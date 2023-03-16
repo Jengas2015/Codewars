f2b1ed53680ea841050d6a8242615e67520d1cd4
Jengas2015 <arejang@gmail.com>
2023-03-16 09:54:02 -0500

    Replace Wtih Alphabet Position. Objects, object.key, includes

---
+++ codes31-40/code32/main.js
@@ -0,0 +1,58 @@
+/* Welcome.
+
+In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc.
+
+Example
+alphabetPosition("The sunset sets at twelve o' clock.")
+Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a string ) */
+
+function alphabetPosition(text) {
+    const textArray = text.toLowerCase().split('')
+
+    const alphabetObject = {
+        a: 1,
+        b: 2,
+        c: 3,
+        d: 4,
+        e: 5,
+        f: 6,
+        g: 7,
+        h: 8,
+        i: 9,
+        j: 10,
+        k: 11,
+        l: 12,
+        m: 13,
+        n: 14,
+        o: 15,
+        p: 16,
+        q: 17,
+        r: 18,
+        s: 19,
+        t: 20,
+        u: 21,
+        v: 22,
+        w: 23,
+        x: 24,
+        y: 25,
+        z: 26
+    }
+    const alphaObjectProps = Object.keys(alphabetObject)
+
+    const alphaToNum = []
+
+    for (let i=0; i <textArray.length; i++) {
+        if (alphaObjectProps.includes(textArray[i])) {
+            alphaToNum.push(alphabetObject[textArray[i]])
+        }
+    }
+    return alphaToNum.join(' ')
+  }
+
+  alphabetPosition("The sunset sets at twelve o' clock.")
+
+  //tweet to explanation https://twitter.com/Codingwithroy/status/1636379382758662151?s=20
