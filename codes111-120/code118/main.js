5c489818ca2bb769c99c15d5b08c571107ef25da
Jengas2015 <arejang@gmail.com>
2023-06-10 15:21:55 -0500

    added explanation tweet.

--- codes111-120/code118/main.js
+++ codes111-120/code118/main.js
@@ -36,16 +36,16 @@ nums2.length == n
 -109 <= nums1[i], nums2[j] <= 109 */
 
 var merge = function (nums1, m, nums2, n) {
-    let i = m - 1;
-    let j = n - 1;
-    let k = m + n - 1;
-    while (j >= 0) {
-        if (i >= 0 && nums1[i] > nums2[j]) {
-            nums1[k--] = nums1[i--];
-        } else {
-            nums1[k--] = nums2[j--];
-        }
+
+    let i= m
+
+    for (j=0; j<nums2.length; j++) {
+        nums1[i++]=nums2[j]
     }
+    
+    nums1.sort((a, b)=> a-b)
 };
 
 console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
+
+//explanation tweet: https://twitter.com/Codingwithroy/status/1667627963779633156?s=20
