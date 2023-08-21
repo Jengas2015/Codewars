a6ae6114d41e1b430d92eaf84850f8e29111a055
Jengas2015 <arejang@gmail.com>
2023-08-21 17:43:40 -0500

    construct binary tree from preorder and inorder traversal. constructs a tree using the aforementioned traversal methods.

---
+++ codes181-190/code188/main.js
@@ -0,0 +1,48 @@
+/* Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+ 
+
+Example 1:
+
+
+Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+Output: [3,9,20,null,null,15,7]
+Example 2:
+
+Input: preorder = [-1], inorder = [-1]
+Output: [-1]
+ 
+
+Constraints:
+
+1 <= preorder.length <= 3000
+inorder.length == preorder.length
+-3000 <= preorder[i], inorder[i] <= 3000
+preorder and inorder consist of unique values.
+Each value of inorder also appears in preorder.
+preorder is guaranteed to be the preorder traversal of the tree.
+inorder is guaranteed to be the inorder traversal of the tree. */
+
+var buildTree = function (preorder, inorder) {
+    if (!preorder.length) return null;
+    const root = new TreeNode(preorder[0]);
+    const index = inorder.indexOf(preorder[0]);
+    root.left = buildTree(preorder.slice(1, index + 1), inorder.slice(0, index));
+    root.right = buildTree(preorder.slice(index + 1), inorder.slice(index + 1));
+    return root;
+}
+
+// build TreeNode from example 1
+
+class TreeNode {
+    constructor(val) {
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const preorder = [3, 9, 20, 15, 7];
+const inorder = [9, 3, 15, 20, 7];
+
+console.log(buildTree(preorder, inorder)); // [3,9,20,null,null,15,7]
