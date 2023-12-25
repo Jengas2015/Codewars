ecaf9ececf73ff9ce2bb5b471e6eea1ad7f05b74
Jengas2015 <arejang@gmail.com>
2023-12-25 08:53:42 -0600

    Word Pattern

---
+++ codes301-310/code10/main.js
@@ -0,0 +1,46 @@
+/* Given a pattern and a string s, find if s follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s.
+
+ 
+
+Example 1:
+
+Input: pattern = "abba", s = "dog cat cat dog"
+Output: true
+Example 2:
+
+Input: pattern = "abba", s = "dog cat cat fish"
+Output: false
+Example 3:
+
+Input: pattern = "aaaa", s = "dog cat cat dog"
+Output: false
+ 
+
+Constraints:
+
+1 <= pattern.length <= 300
+pattern contains only lower-case English letters.
+1 <= s.length <= 3000
+s contains only lowercase English letters and spaces ' '.
+s does not contain any leading or trailing spaces.
+All the words in s are separated by a single space. */
+
+function wordPattern(pattern, s) {
+    let arr = s.split(" ");
+    let map = {};
+    let set = new Set();
+    if (pattern.length !== arr.length) return false;
+    for (let i = 0; i < pattern.length; i++) {
+        if (!map[pattern[i]]) {
+            if (set.has(arr[i])) return false;
+            map[pattern[i]] = arr[i];
+            set.add(arr[i]);
+        } else {
+            if (map[pattern[i]] !== arr[i]) return false;
+        }
+    }
+    return true;
+}
+
