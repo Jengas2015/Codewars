e4b225386a2f0b065d77712a7ac3122504aad97d
Jengas2015 <arejang@gmail.com>
2024-07-09 16:18:47 -0500

    July 1 notes

---
+++ codes461-470/code02/main.js
@@ -0,0 +1,6 @@
+/* 
+What does slicing mean when it comes to the features CSS?
+I can't launch Dragon.
+
+Horvat often codes by referencing methods that don't yet exist, and later building out that method. Is this how things normally play out in a practical setting, or is Horvat just unusally very brilliant, and things play out differently?
+*/
